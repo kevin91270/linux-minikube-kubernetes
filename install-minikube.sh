e38daf9ade53a39e58715292327a833d7e8643ad
kevin91270 <kevin.chapelle@estiam.com>
2020-02-26 09:49:33 +0100

    update sh

--- install-minikube.sh
+++ install-minikube.sh
@@ -1,16 +1,2 @@
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-
-sudo chmod +x ./kubectl
-
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-
-sudo chmod +x minikube
-
-sudo mkdir -p /usr/local/bin/
-
-sudo install minikube /usr/local/bin/
-
-sudo minikube start --vm-driver=none
-
+sudo curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.3/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mkdir -p /usr/local/bin/ && sudo install minikube /usr/local/bin/ && sudo minikube start --vm-driver=none
