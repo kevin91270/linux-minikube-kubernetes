2536d0371e0be9c552014eb67fe2e02e8cc72949
kevin91270 <kevin.chapelle@estiam.com>
2020-02-26 15:40:10 +0100

    final

--- minikube-pods.sh
+++ minikube-pods.sh
@@ -6,17 +6,3 @@ sudo kubectl apply -f ./service-postgres.yml
 sudo kubectl apply -f ./dp-vuejs.yml
 sudo kubectl apply -f ./dp-strapi.yml
 sudo kubectl apply -f ./dp-postgres.yml
-
-sudo apt install gnupg2 pass
-sudo apt-get update
-sudo apt-get install postgresql-client
-
-sudo docker login
-sudo docker build -t kevin91270/vuejs-eval:001 .
-sudo docker run --rm -P --name pg_test kevin91270/vuejs-eval:001
-sudo docker push kevin91270/vuejs-eval:001
-
-sudo docker ps
-
-#psql -h localhost -p "" -d docker -U docker --password
-# il faut verifier le port avec sudo docker ps
