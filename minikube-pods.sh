a65d6c0ed1d523ac14d7c61da35d094c557ce986
kevin91270 <kevin.chapelle@estiam.com>
2021-02-08 23:19:59 +0000

    final

--- minikube-pods.sh
+++ minikube-pods.sh
@@ -6,6 +6,8 @@ sudo kubectl apply -f ./service-mongo.yml
 sudo kubectl apply -f ./dp-vuejs.yml
 sudo kubectl apply -f ./dp-node.yml
 sudo kubectl apply -f ./dp-mongo.yml
+sudo kubectl apply -f ./pv-claim.yml
+sudo kubectl apply -f ./pv-volume.yml
 
 sudo kubectl get services -n eval
 sudo chmod +x kubernetes.sh
